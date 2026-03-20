4a9798345ceeb566f28c0a060bc9b439ec362f31
Luis Emidio <luisfuturist@gmail.com>
2026-03-20 00:42:46 -0300

    feat: add error handler plugin to base pluginpack
    - Integrated the `errorHandlerPlugin` into the base pluginpack, enhancing error management capabilities within the CLI framework.
    - This addition aims to improve user experience by providing better error handling and reporting.

--- src/std/pluginpacks/base.ts
+++ src/std/pluginpacks/base.ts
@@ -1,9 +1,10 @@
 import { definePluginpack } from "~/core/definition/pack";
 import deprecationPlugin from "~/std/plugins/deprecation";
+import errorHandlerPlugin from "~/std/plugins/error-handler";
 import helpPlugin from "~/std/plugins/help";
 import versionPlugin from "~/std/plugins/version";
 
 export default definePluginpack({
     name: "base",
-    plugins: [deprecationPlugin, helpPlugin, versionPlugin],
+    plugins: [deprecationPlugin, helpPlugin, versionPlugin, errorHandlerPlugin],
 });
